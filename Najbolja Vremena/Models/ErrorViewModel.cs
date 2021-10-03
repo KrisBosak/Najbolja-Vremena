5423a9e344b6eb4c419936350951e2b1c9c46d1b
Kris Bosak <kris.bosak@nttdata.com>
2021-10-03 09:38:17 +0200

    Add project files.

---
+++ Najbolja Vremena/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Najbolja_Vremena.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Najbolja Vremena/Models/Vremena.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Najbolja_Vremena.Models
+{
+    public class Vremena
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime Vrijeme { get; set; }
+
+        public Vremena()
+        {
+
+        }
+    }
+}
